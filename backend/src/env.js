383c040e5959cb873455a84e9d163e4e34cd28ae
Padmaja Sharma <97097465+Padmajasharma@users.noreply.github.com>
2025-01-12 21:58:41 +0530

    Update env.js

--- backend/src/env.js
+++ backend/src/env.js
@@ -3,6 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const PORT = process.env.PORT || 3000;
-export const MONGO_URI =
-  process.env.MONGO_URI || "mongodb://localhost:27017/notes";
+export const MONGO_URI = "mongodb+srv://padmaja121003:Mowgli12%40%40assign1.uiwr0.mongodb.net/?retryWrites=true&w=majority&appName=assign1
+"
 export const JWT_SECRET = process.env.JWT_SECRET;
