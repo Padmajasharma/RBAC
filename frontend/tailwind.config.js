9a779e263e324ac27850d813bee68727a31f25e0
Padmaja Sharma <97097465+Padmajasharma@users.noreply.github.com>
2025-01-12 22:03:22 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,14 @@
-import flowbite from "flowbite-react/tailwind";
-
 /** @type {import('tailwindcss').Config} */
+import flowbite from "flowbite/plugin";
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", ...flowbite.content()],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/**/*.js", // Include this if using flowbite-react components
+  ],
   theme: {
     extend: {},
   },
-  plugins: [
-    ...flowbite.plugin(),
-  ],
+  plugins: [flowbite],
 };
