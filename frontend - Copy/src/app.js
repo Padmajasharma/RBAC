ad89a0f8ec3374c37c7a38e1ee9091aff34572c7
Padmaja Sharma <97097465+Padmajasharma@users.noreply.github.com>
2025-01-12 23:37:15 +0530

    Update app.js

--- frontend - Copy/src/app.js
+++ frontend - Copy/src/app.js
@@ -1,18 +1,30 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LoginForm from './components/LoginForm';
+import SignupForm from './components/SignupForm';
 import Home from './pages/Home';
 import Admin from './pages/Admin';
 
 const App = () => {
+  const [token, setToken] = useState(localStorage.getItem('token'));
+
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/admin" element={<Admin />} />
-      </Routes>
+      <div>
+        {!token ? (
+          <div>
+            <LoginForm setToken={setToken} />
+            <SignupForm />
+          </div>
+        ) : (
+          <Switch>
+            <Route path="/admin" component={Admin} />
+            <Route path="/" component={Home} />
+          </Switch>
+        )}
+      </div>
     </Router>
   );
 };
 
 export default App;
-
