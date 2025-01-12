947d4f227c9e3d5b840651496f261effff77b44b
Padmaja Sharma <97097465+Padmajasharma@users.noreply.github.com>
2025-01-12 23:37:46 +0530

    Update Home.js

--- frontend - Copy/src/pages/Home.js
+++ frontend - Copy/src/pages/Home.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import PostList from '../components/PostList';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to the Blog</h1>
+      <PostList />
+    </div>
+  );
+};
+
+export default Home;
 
