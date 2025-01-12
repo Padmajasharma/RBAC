e02788ebe6582a92e9469a89d8160df2825aa586
Padmaja Sharma <97097465+Padmajasharma@users.noreply.github.com>
2025-01-12 23:31:32 +0530

    Update app.js

--- frontend - Copy/src/app.js
+++ frontend - Copy/src/app.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Admin from './pages/Admin';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
 
