7eb9afbf9addb3ae1e7f738a3ab5d25d2a8bc699
Padmaja Sharma <97097465+Padmajasharma@users.noreply.github.com>
2025-01-12 23:37:29 +0530

    Update Admin.js

--- frontend - Copy/src/pages/Admin.js
+++ frontend - Copy/src/pages/Admin.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import AdminDashboard from '../components/AdminDashboard';
 
+const Admin = () => {
+  return (
+    <div>
+      <h1>Admin Panel</h1>
+      <AdminDashboard />
+    </div>
+  );
+};
+
+export default Admin;
